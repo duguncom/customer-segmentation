7546fa25714c884c4436d2389d68bf30cd80b805
isilberfin <isilberfin@hotmail.com>
2023-05-25 11:51:13 +0300

    customers-added

---
+++ sql/payment.sql
@@ -0,0 +1,22 @@
+select
+payment.id as payment_id,
+case when payment.recurring_month is null and amount = 1 then 1 else payment.recurring_month end as recurring_month,
+ca.id as customer_agreement_id,
+date(payment.created_at) as payment_created_at,
+cp.customer_id,
+cp.provider_id,
+status_id as payment_status_id,
+payment_due_date,
+payment_date,
+amount,
+case when payment_due_date >= curdate() then null else
+    (case when status_id = 'cancelled' then null else datediff(payment_date,payment_due_date) end) end as payment_delay
+FROM customer_payments as cp
+INNER JOIN payments as payment on payment.id = cp.payment_id
+INNER JOIN customer_agreements as ca on  ca.id = cp.customer_agreement_id
+WHERE cp.customer_id NOT IN (24126)
+and cp.deleted_at is null
+  #and provider.deleted_at is null
+  and payment.deleted_at is null
+and ca.deleted_at is null
+and payment.payment_due_date >= '2019-01-01';
---
+++ sql/satisfaction.sql
@@ -0,0 +1,29 @@
+select cn.customer_id,
+       cn.provider_id,
+       cn.created_at as satisfaction_date,
+       c.value,
+       c.item_id as value_id
+       from customer_notifications as cn
+inner join (SELECT
+P.customer_id,
+date_add(date_add(LAST_DAY(min(PPP.starts_on)),interval 1 DAY),interval -1 MONTH) as min_date
+            FROM
+            products AS PR
+            JOIN product_feature AS PF ON (PR.id = PF.product_id)
+            JOIN product_price AS PP ON (PR.id = PP.product_id)
+            JOIN provider_product_price AS PPP ON (PP.id = PPP.`product_price_id`)
+            JOIN providers AS P ON (P.id = PPP.provider_id)
+JOIN city AS L ON (P.city_id = L.id)
+JOIN provider_category AS PC ON (P.category_id = PC.id)
+            WHERE
+            PF.deleted_at IS NULL AND
+            PR.deleted_at IS NULL AND
+            PPP.deleted_at IS NULL AND
+            PPP.status = 1 AND
+            P.deleted_at is NULL and
+            P.customer_id != 24126 and
+            (CURDATE() BETWEEN PPP.starts_on AND PPP.ends_on) AND
+            PF.listing_type_id IS NOT NULL
+and PR.is_freemium = 0 and PPP.is_trial = 0
+group by P.customer_id) as cus on cus.customer_id = cn.customer_id and cn.created_at >= cus.min_date
+inner join config as c on cn.satisfaction_status = c.item_id and c.item_type = 'satisfaction_status';
